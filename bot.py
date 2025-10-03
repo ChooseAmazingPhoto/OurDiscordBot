6a57a95fb70d716490ba7ed0d127fb8eb5e0c69e
ccyyoyo <ccyyoyo55@gmail.com>
2025-10-03 19:43:59 +0800

    fix: add dotenv

--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
 import os
+
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
+if TOKEN is None:
+    raise RuntimeError("Missing DISCORD_TOKEN environment variable")
+
 intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
